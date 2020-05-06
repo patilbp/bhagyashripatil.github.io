b589f34c73e7a644fa2f7b286b09ec12d498643c
Bhagyashri Patil <36761520+shri-patil@users.noreply.github.com>
2020-05-07 01:12:07 +0530

    Create pageLoad.js

---
+++ js/pageLoad.js
@@ -0,0 +1,41 @@
+/*	PAGE-LOAD.JS  */
+
+function loadIntroPage()
+{
+    window.location.href = "index.html";
+}
+
+function loadEducPage()
+{
+    window.location.href = "otherPages/education.html";
+}
+
+function loadSkillPage()
+{
+    window.location.href = "otherPages/skills.html";
+}
+
+function loadProjPage()
+{
+    window.location.href = "otherPages/projects.html";
+}
+
+function loadContactPage()
+{
+    window.location.href = "otherPages/contact.html";
+}
+
+function loadResumePage()
+{
+    window.open('resume.html');
+}
+
+function loadRP()
+{
+    window.open('otherPages/resume.html');
+}
+
+function loadCP()
+{
+    window.location.href = "contact.html";
+}
